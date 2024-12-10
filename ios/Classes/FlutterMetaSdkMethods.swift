3e5596b229dbe1523fc672aa88539b937df6ce3d
Ricardo Ramos <quality_contato@hotmail.com>
2024-12-10 15:20:11 -0300

    update ver

--- ios/Classes/FlutterMetaSdkMethods.swift
+++ ios/Classes/FlutterMetaSdkMethods.swift
@@ -8,7 +8,7 @@
 import Foundation
 import FBSDKCoreKit
 import FBSDKCoreKit_Basics
-import FBAudienceNetwork
+//import FBAudienceNetwork
 
 class FlutterMetaSdkMethods {
     static func clearUserData(result: @escaping FlutterResult) {
