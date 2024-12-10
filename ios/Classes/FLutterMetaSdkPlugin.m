6bb844e89170b5d05a3f2477d2a870839bcd4824
Ricardo Ramos <quality_contato@hotmail.com>
2024-12-10 20:12:07 -0300

    update flutter_meta_sdk_no_ad_network

--- ios/Classes/FLutterMetaSdkPlugin.m
+++ ios/Classes/FLutterMetaSdkPlugin.m
@@ -5,7 +5,7 @@
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_meta_sdk-Swift.h"
+#import "flutter_meta_sdk_no_ad_network-Swift.h"
 #endif
 
 @implementation FlutterMetaSdkPlugin
