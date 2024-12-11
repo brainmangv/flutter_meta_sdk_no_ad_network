a7dcae7fd001eb08e353a3d5173ea237068541bf
Ricardo Ramos <quality_contato@hotmail.com>
2024-12-10 21:59:01 -0300

    remove static

--- ios/Classes/SwiftFlutterMetaSdkPlugin.swift
+++ ios/Classes/SwiftFlutterMetaSdkPlugin.swift
@@ -84,12 +84,12 @@ public class SwiftFlutterMetaSdkPlugin: NSObject, FlutterPlugin {
             }
         }
 
-        static func clearUserData(result: @escaping FlutterResult) {
+        func clearUserData(result: @escaping FlutterResult) {
         AppEvents.shared.clearUserData()
         result(nil)
     }
 
-    static func setUserData(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    func setUserData(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let arguments = call.arguments as? [String: Any] ?? [String: Any]()
 
         AppEvents.shared.setUserData(arguments["email"] as? String, forType: FBSDKAppEventUserDataType.email)
@@ -106,25 +106,25 @@ public class SwiftFlutterMetaSdkPlugin: NSObject, FlutterPlugin {
         result(nil)
     }
 
-    static func clearUserID(result: @escaping FlutterResult) {
+    func clearUserID(result: @escaping FlutterResult) {
         AppEvents.shared.userID = nil
         result(nil)
     }
 
-    static func flush(result: @escaping FlutterResult) {
+    func flush(result: @escaping FlutterResult) {
         AppEvents.shared.flush()
         result(nil)
     }
 
-    static func getApplicationId(result: @escaping FlutterResult) {
+    func getApplicationId(result: @escaping FlutterResult) {
         result(Settings.shared.appID)
     }
 
-    static func getAnonymousId(result: @escaping FlutterResult) {
+    func getAnonymousId(result: @escaping FlutterResult) {
         result(AppEvents.shared.anonymousID)
     }
 
-    static func logEvent(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    func logEvent(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let arguments = call.arguments as? [String: Any] ?? [String: Any]()
         let eventName = arguments["name"] as! String
         let parameters = arguments["parameters"] as? [AppEvents.ParameterName: Any] ?? [AppEvents.ParameterName: Any]()
@@ -138,7 +138,7 @@ public class SwiftFlutterMetaSdkPlugin: NSObject, FlutterPlugin {
         result(nil)
     }
 
-    static func pushNotificationOpen(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    func pushNotificationOpen(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let arguments = call.arguments as? [String: Any] ?? [String: Any]()
         let payload = arguments["payload"] as? [String: Any]
         if let action = arguments["action"] {
@@ -151,19 +151,19 @@ public class SwiftFlutterMetaSdkPlugin: NSObject, FlutterPlugin {
         result(nil)
     }
 
-    static func setUserId(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    func setUserId(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let id = call.arguments as! String
         AppEvents.shared.userID = id
         result(nil)
     }
 
-    static func setAutoLogAppEventsEnabled(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    func setAutoLogAppEventsEnabled(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let enabled = call.arguments as! Bool
         Settings.shared.isAutoLogAppEventsEnabled = enabled
         result(nil)
     }
 
-    static func setDataProcessingOptions(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    func setDataProcessingOptions(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let arguments = call.arguments as? [String: Any] ?? [String: Any]()
         let modes = arguments["options"] as? [String] ?? []
         let state = arguments["state"] as? Int32 ?? 0
@@ -174,7 +174,7 @@ public class SwiftFlutterMetaSdkPlugin: NSObject, FlutterPlugin {
         result(nil)
     }
 
-    static func purchased(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    func purchased(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let arguments = call.arguments as? [String: Any] ?? [String: Any]()
         let amount = arguments["amount"] as! Double
         let currency = arguments["currency"] as! String
@@ -184,7 +184,7 @@ public class SwiftFlutterMetaSdkPlugin: NSObject, FlutterPlugin {
         result(nil)
     }
 
-    static func setAdvertiserTracking(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    func setAdvertiserTracking(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let arguments = call.arguments as? [String: Any] ?? [String: Any]()
         let enabled = arguments["enabled"] as! Bool
         let collectId = arguments["collectId"] as! Bool
@@ -193,7 +193,7 @@ public class SwiftFlutterMetaSdkPlugin: NSObject, FlutterPlugin {
         result(nil)
     }
     
-    static func activateApp(){
+    func activateApp(){
         AppEvents.shared.activateApp()
     }
 }
